a4728dcd17d52bab3395764e78000faf01872c3e
yideg <misganewendeg879@gmail.com>
2023-02-16 21:23:16 +0300

    small conf added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome To TypeScript!</h1>
+        <h1>Welcome To TypeScript!</h1>
+        <h1>Dashin Academy Portal</h1>
+        <h1>Vercel Deployment</h1>
     </div>
   );
 }
